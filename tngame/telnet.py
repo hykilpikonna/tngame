06532594c84030ca1e49e235adb01f3705c1cefd
Azalea Gui <me@hydev.org>
2023-03-08 00:50:20 -0500

    [O] Clear path around cat

--- tngame/telnet.py
+++ tngame/telnet.py
@@ -16,9 +16,9 @@ log = setup_logger(DEBUG)
 
 
 ASCII_CAT = r"""
- /\_/\
-( | | )
- >   <""".strip('\n')
+  /\_/\ 
+ ( | | ) 
+  >   < """.strip('\n')
 
 ASCII_HEIGHT = ASCII_CAT.count('\n')
 ASCII_WIDTH = max(len(line.strip('\n')) for line in ASCII_CAT.splitlines())
