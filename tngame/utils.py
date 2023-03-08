369c618d495564a0899aa13a1d53a0c0ae0bee9d
Azalea Gui <me@hydev.org>
2023-03-08 01:59:03 -0500

    [+] Encapsulate ascii dimensions

--- tngame/utils.py
+++ tngame/utils.py
@@ -21,3 +21,10 @@ def setup_logger(debug: bool):
     )
 
     return logging.getLogger("a2")
+
+
+def get_ascii_dimensions(ascii_art: str) -> tuple[int, int]:
+    """Get the dimensions of an ASCII art."""
+    height = ascii_art.count('\n')
+    width = max(len(line.strip('\n')) for line in ascii_art.splitlines())
+    return height, width
